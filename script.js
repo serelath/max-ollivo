77644be8da2c8d69ec499ed502132dee610f339c
serelath <shoh@go.pasadena.edu>
2019-07-12 17:35:18 -0700

    link on insta

--- script.js
+++ script.js
@@ -30,16 +30,16 @@ $(document).ready(function() {
 		$(this).find("rect").removeClass("rect");
 	})
 	
-	function isScrolledIntoView(elem)
-	{
-		var docViewTop = $(window).scrollTop();
-		var docViewBottom = docViewTop + $(window).height();
-
-		var elemTop = $(elem).offset().top;
-		var elemBottom = elemTop + $(elem).height();
-
-		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-	}
+//	function isScrolledIntoView(elem)
+//	{
+//		var docViewTop = $(window).scrollTop();
+//		var docViewBottom = docViewTop + $(window).height();
+//
+//		var elemTop = $(elem).offset().top;
+//		var elemBottom = elemTop + $(elem).height();
+//
+//		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+//	}
 	
 	
 	
